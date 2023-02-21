2576f95db3f819a6cd3027ff415b11410c62bcea
Jasper O'Leary <jhaazpr@gmail.com>
2023-02-21 12:43:23 -0800

    update example1 and rename folder

--- p5-examples-and-addons/button-press/sketch.js
+++ p5-examples-and-addons/button-press/sketch.js
@@ -1,5 +1,5 @@
 var serial; // variable to hold an instance of the serialport library
-var portName = '/dev/ttyACM0' //rename to the name of your port
+var portName = '/dev/tty.usbmodem14301' //rename to the name of your port
 var datain; //some data coming in over serial!
 var xPos = 0;
 
@@ -12,13 +12,13 @@ function setup() {
   serial.on('data', serialEvent);     // callback for when new data arrives
   serial.on('error', serialError);    // callback for errors
   serial.on('close', portClose);      // callback for the port closing
- 
+
   serial.list();                      // list the serial ports
   serial.open(portName);              // open a serial port
   createCanvas(1200, 800);
   background(0x08, 0x16, 0x40);
 }
- 
+
 // get the list of ports:
 function printList(portList) {
  // portList is an array of serial port names
@@ -31,24 +31,28 @@ function printList(portList) {
 function serverConnected() {
   print('connected to server.');
 }
- 
+
 function portOpen() {
   print('the serial port opened.')
 }
- 
+
 function serialError(err) {
-  print('Something went wrong with the serial port. ' + err);
+  //print('Something went wrong with the serial port. ' + err);
+  print(err);
 }
- 
+
 function portClose() {
   print('The serial port closed.');
 }
 
 function serialEvent() {
   if (serial.available()) {
-  	datain = Number(serial.readLine());
-        //console.log(datain);
-  } 
+    let maybeNumber = parseInt(serial.readLine());
+    if (!isNaN(maybeNumber)) {
+        datain = maybeNumber;
+        console.log(datain);
+    }
+  }
 }
 
 function graphData(newData) {
